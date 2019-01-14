62adbd7760ae8d3d3173e8d2abd9ce0be8fa112e
Danny Sepler <dsepler@ufl.edu>
2019-01-13 23:05:07 -0500

    Updated Resume

--- scripts/home.js
+++ scripts/home.js
@@ -1,29 +1,26 @@
-PDFObject.embed("myresume.pdf", "#show-resume-here");
+PDFObject.embed("Danny Sepler | Resume.pdf", "#show-resume-here");
 
-$( "#resume-plus" ).click(function() {
-  $( "#show-resume-here" ).toggle();
+$("#resume-plus").click(function() {
+  $("#show-resume-here").toggle();
 
-  var text = $(this).text();
-  $(this).text(text == "(+)" ? "(-)" : "(+)");
+  const text = $(this).text();
+  $(this).text(text === "(+)" ? "(-)" : "(+)");
 });
 
-$( '#go-gators' ).hover(function() {
-	// $(' #gator-img' ).toggleClass('invisible');
-	$(' #go-gators-name' ).toggleClass('bold');
-	$(' #go' )	     .toggleClass('go-gators-orange');
-	$(' #gators' )   .toggleClass('go-gators-blue');
+$('#go-gators').hover(function() {
+	$('#go-gators-name').toggleClass('bold');
+	$('#go')	.toggleClass('go-gators-orange');
+	$('#gators').toggleClass('go-gators-blue');
 });
 
-$( '#american-express' )    .hover(function() {
-	// $(' #amex-img' ).toggleClass('invisible');
-	$(' #amex-name' ).toggleClass('bold');
-	$(' #american' )	    .toggleClass('amex-blue');
+$( '#american-express' ).hover(function() {
+	$('#amex-name').toggleClass('bold');
+	$('#american') .toggleClass('amex-blue');
 	// The other amex color is white, no reason to modify it
 });
 
 $( '#flatiron-health' ).hover(function() {
-	// $(' #flatiron-img' ).toggleClass('invisible');
-	$(' #fh-name' ).toggleClass('bold');
-	$(' #flatiron' )       .toggleClass('fh-purple');
-	$(' #health' )	       .toggleClass('fh-blue');
+	$('#fh-name') .toggleClass('bold');
+	$('#flatiron').toggleClass('fh-purple');
+	$('#health')  .toggleClass('fh-blue');
 });
