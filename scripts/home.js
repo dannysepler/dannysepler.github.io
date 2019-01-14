48dc7f8664553ea35566fe80fb18d90eb2c6f398
Danny Sepler <dsepler@ufl.edu>
2019-01-13 19:14:59 -0500

    Better site icon, fancy SVGs pop up on milestone hover

--- scripts/home.js
+++ scripts/home.js
@@ -8,18 +8,21 @@ $( "#resume-plus" ).click(function() {
 });
 
 $( '#go-gators' ).hover(function() {
+	$(' #gator-img' ).toggleClass('invisible');
 	$(' #go-gators-name' ).toggleClass('bold');
 	$(' #go' )	     .toggleClass('go-gators-orange');
 	$(' #gators' )   .toggleClass('go-gators-blue');
 });
 
 $( '#american-express' )    .hover(function() {
+	$(' #amex-img' ).toggleClass('invisible');
 	$(' #amex-name' ).toggleClass('bold');
 	$(' #american' )	    .toggleClass('amex-blue');
 	// The other amex color is white, no reason to modify it
 });
 
 $( '#flatiron-health' ).hover(function() {
+	$(' #flatiron-img' ).toggleClass('invisible');
 	$(' #fh-name' ).toggleClass('bold');
 	$(' #flatiron' )       .toggleClass('fh-purple');
 	$(' #health' )	       .toggleClass('fh-blue');
